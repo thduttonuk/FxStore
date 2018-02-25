df4ecf2a8cb6f1d094761bf13a77821ff86b5db8
Tom Dutton <tom.dutton@outlook.com>
2018-02-25 20:42:36 +0000

    Command test added

--- src/store/store.spec.ts
+++ src/store/store.spec.ts
@@ -8,13 +8,20 @@ interface State {
 
 const initialStore1State: State = {
   IsLoading: false,
-  IsBusy: true
+  IsBusy: false
 }
 
 @Command(initialStore1State)
 class Store1Command {
   public Handle(state: State) {
     state.IsLoading = true;
+  }
+}
+
+@Command(initialStore1State)
+class Store2Command {
+  public Handle(state: State) {
+    state.IsBusy = true;
   }
 }
 
@@ -33,25 +40,20 @@ test('should throw if class is not named Command', () => {
 
 test('should add dispatch command name', () => {
   const store1Command = new Store1Command();
-  console.log(store1Command)
   expect((<any>store1Command).CommandName).toBe('Store1Command')
 });
 
-test('should change state', () => {
+test('should change is loading', () => {
   const store1Command: any = new Store1Command();
   store1Command.Dispatch();
 
   expect(initialStore1State.IsLoading).toBeTruthy();
-  expect(initialStore1State.IsBusy).toBeTruthy();
 });
 
-test('should change state', () => {
-  const store1Command1: any = new Store1Command();
-  const store1Command2: any = new Store1Command();
-  const store1Command3: any = new Store1Command();
-  store1Command1.Dispatch();
+test('should change is busy', () => {
+  const store2Command: any = new Store2Command();
+  store2Command.Dispatch();
 
-  expect(initialStore1State.IsLoading).toBeTruthy();
   expect(initialStore1State.IsBusy).toBeTruthy();
 });
 
